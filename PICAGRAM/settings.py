9c70ba9f12be8e83403bbd70f7be692a7cb194ea
Joonas Timlin <joonas@timlins.se>
2023-12-28 12:30:14 +0000

    debuggin

--- PICAGRAM/settings.py
+++ PICAGRAM/settings.py
@@ -60,7 +60,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
    os.environ.get('ALLOWED_HOST'),
