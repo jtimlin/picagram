25bdcbc998648edad20a4e014db9b2338e103046
Joonas Timlin <joonas@timlins.se>
2023-12-28 12:07:42 +0000

    Add Procfile and final deployment fix

--- PICAGRAM/settings.py
+++ PICAGRAM/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-jindah-picagram-5d6w3yhzals.ws-eu107.gitpod.io']
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
