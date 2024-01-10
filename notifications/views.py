5ba78e4b29ee587d76b7ef15a6a339ebce2087a8
Joonas Timlin <joonas@timlins.se>
2024-01-10 20:24:37 +0000

    Add views and permissions to notifications app

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from PICAGRAM.permissions import IsNotificationOwner
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+
+class NotificationList(generics.ListAPIView):
+    """List a user's notifications. No create view, since notifications are
+    automatically created.
+    """
+
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["sent_at", "is_read"]
+
+    def get_queryset(self):
+        return Notification.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class NotificationDetail(generics.RetrieveUpdateDestroyAPIView):
+    """Retrieve or delete a notification by id.
+    Restricted to notification owner.
+    """
+
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated, IsNotificationOwner]
